a5bd7c8f6d8c561aa875c5531f41b2d31b7d5e74
Damian Rebolo <drebolo@ohpen.nl>
2020-10-16 11:33:00 +0200

    test name changed

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { store } from './store';
 import App from './App';
 
-test('renders learn react link', () => {
+test('<App />', () => {
   const { getByText } = render(
     <Provider store={store}>
       <App />
