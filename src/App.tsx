568bb884e75eb433b08deae8509490d5f02b65b7
Damian Rebolo <drebolo@ohpen.nl>
2020-10-16 15:40:17 +0200

    styled component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const PrimaryButton = styled.button`
+  background-color: blue;
+  padding: 5px;
+`;
 
 function App(): JSX.Element {
-  return <div className="App">App working!</div>;
+  return (
+    <div className="App">
+      App working! <PrimaryButton>Primary Button</PrimaryButton>
+    </div>
+  );
 }
 
 export default App;
