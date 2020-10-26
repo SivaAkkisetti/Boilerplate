4de87d9ba606e79023dd15abcdbdfc3056de1cf1
Damian Rebolo <drebolo@ohpen.nl>
2020-10-26 16:57:45 +0100

    change plugin

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -17,6 +17,7 @@
  */
 module.exports = (on, config) => {
   require('@cypress/code-coverage/task')(on, config);
-  on('file:preprocessor', require('@cypress/code-coverage/use-browserify-istanbul'));
+  // IMPORTANT to return the config object
+  // with the any changed environment variables
   return config;
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,7 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const PrimaryButton = styled.button`
-  background-color: blue;
-  padding: 5px;
-`;
 
 function App(): JSX.Element {
-  return (
-    <div className="App">
-      App working! <PrimaryButton>Primary Button</PrimaryButton>
-    </div>
-  );
+  return <div className="App">App working!</div>;
 }
 
 export default App;
